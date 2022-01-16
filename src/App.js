3f5124ca3512dc15d88231ab696ade2a17b00784
raharad28 <97815804+raharad28@users.noreply.github.com>
2022-01-15 23:40:17 -0800

    bebas

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="Header">header</div>
+
+      <div className="section">
+        <div className="card section-1">1</div>
+        <div className="card section-2">2</div>
+        <div className="card section-3">3</div>
+        <div className="card section-4">4</div>
+        <div className="card section-5">5</div>
+        <div className="card section-6">6</div>
+    </div>
+
+    <div className="Body">
+      <div className="Body-Anak Body-1"></div>
+      <div className="Body-Anak Body-2"></div>
+      <div className="Body-Anak Body-3"></div>
+      <div></div> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
